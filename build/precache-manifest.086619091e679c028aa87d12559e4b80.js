f02070358397488c6e441ead703d5ce2190c3c30
dpikachan07 <chandrasekardeepika07@gmail.com>
2019-05-06 23:02:13 +0100

    React changes

---
+++ build/precache-manifest.086619091e679c028aa87d12559e4b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e60013f0d5f38555b4afb3a66abdab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86667f924fc89157e982",
+    "url": "/static/css/main.ce822890.chunk.css"
+  },
+  {
+    "revision": "5d4f15d4ffb23793028d",
+    "url": "/static/js/2.e9228e71.chunk.js"
+  },
+  {
+    "revision": "86667f924fc89157e982",
+    "url": "/static/js/main.5b3667c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import './LandingPage.css';
+import './NavBar.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const LandingPage = () =>{
+  return(<div className="landing-text">
+      <h1>Grange Mobile App</h1>
+      <img src ="../images/logo.jpeg" alt="Tu Dublin Logo" className="logo" />
+      <div className="container">
+        <button type="button" className="lecturer"><b>Lecturer</b></button>
+        <button type="button" className="student"><b>Student</b></button>
+      </div>
+  </div>);
+};
+
+const HomePage = () => {
+  return(
+
+    <div className="container">
+        <div className="row">
+            <div className="col">
+                <img src="images/TUDBanner.jpg" className="logo"/>
+            </div>
+        </div>
+    </div>
+
+  );
+};
+const NavBar = (props) =>{
+  return(
+
+      <nav className="navbar navbar-inverse">
+      <div className="container-fluid">
+      <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">TU Dublin Grange Mobile App</a>
+      </div>
+      <div className="collapse navbar-collapse" id="myNavbar">
+          <ul className="nav navbar-nav">
+           <li><a href="Home.html"><span className="glyphicon glyphicon-home"></span> Home </a></li>
+           <li><a className="isDisabled" href="#">Exam Time Table</a></li>
+           <li><a href="MyCourses.html">My Course</a></li>
+
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+           <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Logout</a></li>
+          </ul>
+      </div>
+      </div>
+      </nav>
+
+  );
+};
+
+class App extends React.Component{
+  render(){
+    return(
+        <div>
+          <BrowserRouter>
+            <div>
+            <Route path="/" exact component={LandingPage} />
+            <Route path="/home" component={HomePage} />
+            </div>
+          </BrowserRouter>
+        </div>
+    );
+  };
+};
+
+export default App;
