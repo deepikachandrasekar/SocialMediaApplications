809424c250a80af4582e93de68f06ba9465e8e0c
dpikachan07 <chandrasekardeepika07@gmail.com>
2019-05-07 12:51:29 +0100

    React changes

---
+++ src/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './LandingPage.css';
+
+
+const HomePage = () => {
+  return(
+
+    <div className="container">
+        <div className="row">
+            <div className="col">
+                <img src="../images/TUDBanner.jpg" className="logo"/>
+            </div>
+        </div>
+    </div>
+
+  );
+}
+
+export default HomePage;
